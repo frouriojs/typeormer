5f867557f938132892cf7f6604806d24f83e9750
solufa <solufa2020@gmail.com>
2020-08-12 01:45:08 +0900

    first commit

---
+++ src/buildTemplate.ts
@@ -0,0 +1,8 @@
+import path from 'path'
+import { Config } from './getConfig'
+
+export default ({ output }: Config) => {
+  const text = '/* eslint-disable */'
+
+  return { text, filePath: path.posix.join(output, '$template.ts') }
+}
