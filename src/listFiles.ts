8233fec7afb91338507550fab8b8ea3f00ad9845
solufa <solufa2020@gmail.com>
2020-11-07 12:37:26 +0900

    fix: remove flatMap for node v10

--- src/listFiles.ts
+++ src/listFiles.ts
@@ -2,9 +2,9 @@ import fs from 'fs'
 import path from 'path'
 
 export const listFiles = (targetDir: string): string[] =>
-  fs.readdirSync(targetDir, { withFileTypes: true }).flatMap(dirent => {
+  fs.readdirSync(targetDir, { withFileTypes: true }).reduce<string[]>((prev, dirent) => {
     const target = path.posix.join(targetDir, dirent.name)
     return dirent.isFile()
-      ? [targetDir.startsWith('.') ? `./${target}` : target]
-      : listFiles(target)
-  })
+      ? [...prev, targetDir.startsWith('.') ? `./${target}` : target]
+      : [...prev, ...listFiles(target)]
+  }, [])
